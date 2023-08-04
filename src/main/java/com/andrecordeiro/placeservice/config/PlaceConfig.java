4bce595aefb84a4b9207c0ac726e8cbf9bd5c34c
andrecordeirolima <andrecordeirolima@gmail.com>
2023-08-04 13:24:56 -0300

    Date and name generation settings

--- src/main/java/com/andrecordeiro/placeservice/config/PlaceConfig.java
+++ src/main/java/com/andrecordeiro/placeservice/config/PlaceConfig.java
@@ -4,8 +4,10 @@ import com.andrecordeiro.placeservice.domain.PlaceRepository;
 import com.andrecordeiro.placeservice.domain.PlaceService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     @Bean
